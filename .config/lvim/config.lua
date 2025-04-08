950073b8b97e4476f9ea3b09fcc69e96c7ceac08
Santi Hernandez <santi@santihdzs.com>
2025-04-08 16:31:29 -0600

    Updated lvim config.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -41,6 +41,38 @@ lvim.keys.normal_mode["<leader>t"] = ":lua ToggleTermSplit()<CR>"
 -- Make Escape exit terminal mode easily
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 
--- Leader + R to compile and run (C++)
-lvim.keys.normal_mode["<leader>r"] = ":lua vim.cmd('lcd %:p:h | w! | !g++ % -o %:r && ./%:r')<CR>"
+-- Leader + R to run
+lvim.keys.normal_mode["<leader>r"] = ":lua run_program()<CR>"
+function run_program()
+  local filename = vim.fn.expand("%:t")
+  local ext = vim.fn.expand("%:e")
+  
+  if ext == "cpp" then
+    -- C++, g++ 
+    vim.cmd("lcd %:p:h | w! | belowright split | term g++ -std=c++20 % -o %:r && ./%:r")
+  elseif ext == "py" then
+    -- Python, uv
+    vim.cmd("lcd %:p:h | w! | belowright split | term uv run %")
+  else
+    -- error message
+    print("Unsupported file type")
+  end
+end
 
+-- Check virtual environment (python)
+local venv_python_path = vim.fn.getcwd() .. "/.venv/bin/python" 
+-- Set Python interpreter for LSP
+vim.g.python3_host_prog = venv_python_path
+-- pyright
+require'lspconfig'.pyright.setup{
+  settings = {
+    python = {
+      pythonPath = venv_python_path,
+    }
+  }
+}
+
+-- Set default indentation for all files
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
